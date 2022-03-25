ddf2a8b7b7e6d5bb966ced647f48518b3cee60e8
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2022-03-25 21:35:40 +0530

    Create running-sum-of-array.py

---
+++ running-sum-of-array.py
@@ -0,0 +1,14 @@
+from black import out
+
+
+nums = [1,2,3,4]
+# Expected output: [1,3,6,10]
+
+output =[]
+for i in range(0,len(nums)):
+    if i == 0:
+        output.append(nums[i])
+    else: 
+        output.append(output[i-1] + nums[i])
+
+print(output)
